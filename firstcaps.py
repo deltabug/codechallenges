ecb72f0a488de84e0cb8dc282407f11e6179954a
Tamsin <deltabug@users.noreply.github.com>
2019-02-01 12:23:51 +0000

    Add files via upload

---
+++ firstcaps.py
@@ -0,0 +1,5 @@
+def LetterCaps(str):
+    x = str.title()
+    print x
+
+print LetterCaps(raw_input())
---
+++ reverse.py
@@ -0,0 +1,7 @@
+def FirstReverse(str): 
+    instring = str
+    revstring = instring[::-1]
+    return revstring
+    
+# keep this function call here  
+print FirstReverse(raw_input()) 
